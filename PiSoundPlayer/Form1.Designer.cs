d7400237b0ee74667f9be5e825fe269713cf642e
sim1222 <kousuke11236124@gmail.com>
2020-08-05 13:11:22 +0900

    V1

---
+++ PiSoundPlayer/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace PiSoundPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.playbutton = new System.Windows.Forms.Button();
+            this.filebutton = new System.Windows.Forms.Button();
+            this.filebox = new System.Windows.Forms.ComboBox();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.reload = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playbutton
+            // 
+            this.playbutton.Location = new System.Drawing.Point(12, 42);
+            this.playbutton.Name = "playbutton";
+            this.playbutton.Size = new System.Drawing.Size(254, 125);
+            this.playbutton.TabIndex = 0;
+            this.playbutton.Text = "再生";
+            this.playbutton.UseVisualStyleBackColor = true;
+            this.playbutton.MouseDown += new System.Windows.Forms.MouseEventHandler(this.playbutton_MouseDown);
+            this.playbutton.MouseUp += new System.Windows.Forms.MouseEventHandler(this.playbutton_MouseUp);
+            // 
+            // filebutton
+            // 
+            this.filebutton.Location = new System.Drawing.Point(213, 13);
+            this.filebutton.Name = "filebutton";
+            this.filebutton.Size = new System.Drawing.Size(53, 23);
+            this.filebutton.TabIndex = 1;
+            this.filebutton.Text = "参照";
+            this.filebutton.UseVisualStyleBackColor = true;
+            this.filebutton.Click += new System.EventHandler(this.filebutton_Click);
+            // 
+            // filebox
+            // 
+            this.filebox.FormattingEnabled = true;
+            this.filebox.Location = new System.Drawing.Point(12, 13);
+            this.filebox.Name = "filebox";
+            this.filebox.Size = new System.Drawing.Size(195, 20);
+            this.filebox.TabIndex = 2;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(1, 173);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(212, 45);
+            this.axWindowsMediaPlayer1.TabIndex = 3;
+            // 
+            // reload
+            // 
+            this.reload.Location = new System.Drawing.Point(213, 184);
+            this.reload.Name = "reload";
+            this.reload.Size = new System.Drawing.Size(53, 23);
+            this.reload.TabIndex = 4;
+            this.reload.Text = "リロード";
+            this.reload.UseVisualStyleBackColor = true;
+            this.reload.Click += new System.EventHandler(this.reload_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 219);
+            this.Controls.Add(this.reload);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.filebox);
+            this.Controls.Add(this.filebutton);
+            this.Controls.Add(this.playbutton);
+            this.Name = "Form1";
+            this.Text = "PiSoundPlayer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playbutton;
+        private System.Windows.Forms.Button filebutton;
+        private System.Windows.Forms.ComboBox filebox;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button reload;
+    }
+}
---
+++ PiSoundPlayer/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace PiSoundPlayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filebox.Text = Properties.Settings.Default.filepath;
+            axWindowsMediaPlayer1.settings.autoStart = false;
+            axWindowsMediaPlayer1.URL = filebox.Text;
+        }
+        public void filebutton_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialogクラスのインスタンスを作成
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            //デフォルトファイル名
+            ofd.FileName = "ピー音.wav";
+            //デフォルトフォルダ名
+            ofd.InitialDirectory = @"C:\";
+            //ファイル拡張子指定
+            ofd.Filter = "音声ファイル(*.wav;*.mp3)|*.wav;*.mp3|すべてのファイル(*.*)|*.*";
+            //フィルターデフォ
+            ofd.FilterIndex = 1;
+            //窓タイトル
+            ofd.Title = "音声ファイルを選択してください";
+            //現在位置復元
+            ofd.RestoreDirectory = true;
+
+            //ダイアログ表示
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                filebox.Text = ofd.FileName;
+                axWindowsMediaPlayer1.settings.autoStart = false;
+                axWindowsMediaPlayer1.URL = filebox.Text;
+                Properties.Settings.Default.filepath = filebox.Text;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+
+        private void playbutton_MouseDown(object sender, MouseEventArgs e)
+        {
+            //再生
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+        }
+
+        private void playbutton_MouseUp(object sender, MouseEventArgs e)
+        {
+            //再生停止
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+        }
+
+        private void reload_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.settings.autoStart = false;
+            axWindowsMediaPlayer1.URL = filebox.Text;
+        }
+
+
+    }
+}
